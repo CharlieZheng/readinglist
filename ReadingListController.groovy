a1267a71ece3bb4a8f292687d1ae402c83e8039b
Zheng Hanrong <hanrongzheng@gmail.com>
2018-12-03 17:15:23 +0800

    Initial

---
+++ ReadingListController.groovy
@@ -0,0 +1,23 @@
+@Controller
+@RequestMapping("/")
+class ReadingListController {
+    String reader = "Craig"
+    @Autowired
+    ReadingListRepository readingListRepository // ←--- 注入 ReadingListRepository
+
+    @RequestMapping(method = RequestMethod.GET)
+    def readersBooks(Model model) {
+        List<Book> readingList = readingListRepository.findByReader(reader) // ←--- 获取阅读列表
+        if (readingList) {
+            model.addAttribute("books", readingList) // ←--- 设置模型
+        }
+        "readingList" // ←--- 返回视图名称
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    def addToReadingList(Book book) {
+        book.setReader(reader)
+        readingListRepository.save(book) // ←--- 保存图书
+        "redirect:/" // ←--- POST 后重定向
+    }
+}
